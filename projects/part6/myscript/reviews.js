ba98a646e8c33cbf5f9e848cb94cf18c6882a98c
Sboakye-uofsc <SBOAKYE@email.sc.edu>
2025-10-08 23:21:19 -0400

    created 242 file and home page

--- projects/part6/myscript/reviews.js
+++ projects/part6/myscript/reviews.js
@@ -12,6 +12,13 @@ class ReviewGame {
 	}
 
 	createReviewCard(){
+		const urlParams = new URLSearchParams(window.location.search);
+		const gameName = urlParams.get('game');
+
+		if(this.name_game !== gameName) {
+			return null;
+		}
+		
 		const div = document.createElement("div");
 		div.classList.add("container");
 
@@ -23,31 +30,77 @@ class ReviewGame {
 		section.classList.add("spilt-rating");
 		div.append(section);
 
-		const img = document.createElement("img"); /*Change to castle*/
+		const img = document.createElement("img");
 		img.classList.add("img-rate");
-		if(this.name_game === "Castle Crashers") {
-			img.src =`https://sboakye-uofsc.github.io/projects/part6${this.img.substring(1)}`;
-		}
+		img.src = `https://sboakye-uofsc.github.io/projects/part6${this.img.substring(1)}`;
 		img_section.append(img);
 
 		const h2 = document.createElement("h2");
 		h2.classList.add("title-c")
-		if(this.name_game === "Castle Crashers"){
-			h2.innerHTML = this.name_game;
-		}
+		h2.innerHTML = this.name_game;
 		section.append(h2);
 
-		const h3 = document.createElement("h3");
-		if(this.rating === 4.5){
-			h3.innerHTML = this.rating;
-		}
-		section.append(h3);
-
 		const h4 = document.createElement("h4");
-		if(this.released === "2012-26-09"){
-			h4.innerHTML = this.released;
-		}
+		h4.innerHTML = this.released;
 		section.append(h4);
+
+		const rating_prog = document.createElement("div");
+		rating_prog.classList.add("rating_progress");
+		section.append(rating_prog);
+
+		const h3 = document.createElement("h3");
+		h3.innerHTML = this.rating;
+		rating_prog.append(h3);
+		
+		const outerStar = document.createElement("div");
+		outerStar.classList.add("outer-star");
+		rating_prog.append(outerStar);
+
+		const innerStar = document.createElement("div");
+		innerStar.classList.add("inner-star");
+		innerStar.innerHTML = "&#9733;&#9733;&#9733;&#9733;&#9733;";
+		outerStar.append(innerStar);
+
+		const p = document.createElement("p");
+		p.classList.add("rate-num");
+		p.innerHTML = "9,006";
+		rating_prog.append(p);
+
+		const divComment = document.createElement("div"); 
+		divComment.classList.add("divComment");
+		div.append(divComment);
+
+		const respondSection = document.createElement("section");
+		respondSection.classList.add("date_respond");
+		divComment.append(respondSection);
+
+		const userDate = document.createElement("p");
+		userDate.innerHTML = this.date_respond;
+		respondSection.append(userDate);
+
+		const theUsername = document.createElement("section");
+		theUsername.classList.add("user_Name");
+		divComment.append(theUsername);
+
+		const username = document.createElement("h3");
+		username.innerHTML = this.username;
+		theUsername.append(username);
+
+		const descriptionSection = document.createElement("section");
+		descriptionSection.classList.add("desc-Section");
+		divComment.append(descriptionSection);
+
+		const description = document.createElement("p");
+		description.innerHTML = this.description;
+		descriptionSection.append(description);
+
+		const reviewsSection = document.createElement("section");
+		reviewsSection.classList.add("reviewsSection");
+		divComment.append(reviewsSection);
+
+		const reviews = document.createElement("p");
+		reviews.innerHTML = this.reviews;
+		reviewsSection.append(reviews);
 
 		return div;
 	}
@@ -65,18 +118,19 @@ const getReview = async () => {
 };
 
 const showReview = async () => {
-	let reviews = await getReview();
-	let reviewGame = document.getElementById("review-game")
+    let reviews = await getReview();
+    let reviewGame = document.getElementById("review-game")
 
-	reviews.forEach((review)=> {
-		reviewGame.append(getReviewComment(review)); 
-		const reviewInstance = new ReviewGame(review);
-		const card = reviewInstance.createReviewCard();
-
-		reviewGame.append(card);
-	});
-	
+    reviews.forEach((review)=> {
+        const reviewInstance = new ReviewGame(review);
+        const card = reviewInstance.createReviewCard();
+        
+        if(card) {
+            reviewGame.append(card);
+        }
+    });
 }
+
 
 const getReviewComment = (review) => {
 	let section = document.createElement("section");
